9d5b81254535967c4ca70d2c3dcc1d3c701ba60e
Tommie Gannert <tommie@gannert.se>
2025-12-03 17:24:50 +0100

    Defaults to a 1.5% + 20% withdrawal scheme.

--- src/stores/calculator.ts
+++ src/stores/calculator.ts
@@ -14,8 +14,8 @@ export const useCalculatorStore = defineStore('calculator', () => {
   const initialCapital = ref(5000000)
   const development = ref(0.1299) // Swedbank Robur Globalfond A
   const developmentStdDev = ref(0.202) // Swedbank Robur Globalfond A
-  const balanceWithdrawalRate = ref(0.03)
-  const profitWithdrawalRate = ref(0.0)
+  const balanceWithdrawalRate = ref(0.015)
+  const profitWithdrawalRate = ref(0.2)
   const profitLookbackYears = ref(5)
   const iskTaxRate = ref(0.0296)
   const iskTaxRateStdDev = ref(0.005)
